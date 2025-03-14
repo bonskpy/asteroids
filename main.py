f42b3f52b7f040f93faaa22ddeb91ac2453ab8fb
bonskpy <bonskpy@gmail.com>
2025-03-14 20:41:32 +0100

    added fps restriction in game loop

--- main.py
+++ main.py
@@ -8,6 +8,8 @@ from constants import *
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
 
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
@@ -23,6 +25,7 @@ def main():
                 return
         screen.fill((0, 0, 0))
         pygame.display.flip()
+        dt = clock.tick(60) / 1000
 
 
 if __name__ == "__main__":
