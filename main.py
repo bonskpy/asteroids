9321d16346abebafe875d6550329761cf8f749a9
bonskpy <bonskpy@gmail.com>
2025-03-14 21:11:06 +0100

    added player.py & Player class, player is drown in the screen

--- main.py
+++ main.py
@@ -3,30 +3,37 @@
 # throughout the file
 
 import pygame
+from player import *
 from constants import *
 
 def main():
-    pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
     dt = 0
+
+    x = SCREEN_WIDTH / 2
+    y = SCREEN_HEIGHT / 2
+
+    player = Player(x, y)
 
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
 
+    pygame.init()
     init_check = pygame.get_init()
     print(f"Pygame is currently initialized: {init_check}")
 
-
+    # game loop
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill((0, 0, 0))
-        pygame.display.flip()
+        player.draw(screen)
         dt = clock.tick(60) / 1000
 
+        pygame.display.flip()
 
 if __name__ == "__main__":
     main()
